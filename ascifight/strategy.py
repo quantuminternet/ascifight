34905069bc12a43cf0f9b044189b0d6155b20d7e
Paul Kraus <paul.kraus@vodafone.com>
2023-09-19 11:24:43 +0200

    aimp

--- ascifight/strategy.py
+++ ascifight/strategy.py
@@ -1,28 +1,33 @@
 import actor_strategies.actors
 import structlog
-
+import ascifight.computations as computations
 
 logger = structlog.get_logger()
 
+SERVER = "http://sessionstation.de/"
+logger = structlog.get_logger()
 
-def create_actor(remote_actor):
-    strategy = get_strategy(remote_actor)
+TEAM = "EverythingsAwesome"
+PASSWORD = "VFRules"
+
+def create_actor(remote_actor, client):
+    strategy = get_strategy(remote_actor, client)
     logger.info('Creating actor for remote actor', remote_actor=remote_actor['ident'])
     return actor_strategies.actors.Actor(strategy=strategy, actor_id=remote_actor['ident'])
 
-
-def get_strategy(remote_actor):
+def get_strategy(remote_actor, client):
     if remote_actor['ident'] == 0:
-        return GetFlagStrategy(target='TimeOut')
+        return GetFlagStrategy(target='TimeOut', client=client)
     elif remote_actor['ident'] == 1:
-        return GetFlagStrategy(target='Superdetractors')
+        return GetFlagStrategy(target='Superdetractors', client=client)
     elif remote_actor['ident'] == 2:
-        return GetFlagStrategy(target='ByteMe')
+        return GetFlagStrategy(target='ByteMe', client=client)
 
 
 class GetFlagStrategy:
-    def __init__(self, target: str):
+    def __init__(self, target: str, client):
         self.target = target
+        self.client = client
 
     def execute(self, gamestate):
         # this teams flag we want to get
@@ -49,10 +54,10 @@ class GetFlagStrategy:
             # we need to stop if we are standing right next to the base
             if compute_distance(origin=actor_coordinates, target=target_coordinates) == 1:
                 # and grab the flag, the direction is the one we would have walked to
-                issue_order(order="grabput", actor_id=actor["ident"], direction=direction)
+                issue_order(client=self.client,order="grabput", actor_id=actor["ident"], direction=direction)
             # if we are not there yet we need to go
             else:
-                issue_order(order="move", actor_id=actor["ident"], direction=direction)
+                issue_order(client=self.client, order="move", actor_id=actor["ident"], direction=direction)
         # if it has the flag we need to head home
         else:
             # where is home?
@@ -60,4 +65,22 @@ class GetFlagStrategy:
                 origin=actor_coordinates, target=home_coordinates
             )[0]
 
+def to_coordinates(input_coords: dict):
+    return computations.Coordinates(x=input_coords['x'], y=input_coords['y'])
 
+
+def compute_direction(origin, target) -> list[computations.Directions]:
+    return computations.calc_target_coordinate_direction(to_coordinates(origin), to_coordinates(target))
+
+
+def compute_distance(origin, target) -> int:
+    return computations.distance(to_coordinates(origin), to_coordinates(target))
+
+
+def issue_order(client, order: str, actor_id: str, direction: computations.Directions):
+    logger.info("Issuing order", order=order, actor_id=actor_id, direction=direction)
+    client.post(
+        url=f"{SERVER}orders/{order}/{actor_id}",
+        params={"direction": direction.value},
+        auth=(TEAM, PASSWORD),
+    )
