0959aa8b2cee265bea06c126448a1e95c06356dc
Paul Kraus <paul.kraus@vodafone.com>
2023-09-19 10:55:37 +0200

    pass actor identification to ctor.

--- ascifight/strategy.py
+++ ascifight/strategy.py
@@ -8,7 +8,7 @@ logger = structlog.get_logger()
 def create_actor(remote_actor):
     strategy = get_strategy(remote_actor)
     logger.info('Creating actor for remote actor', remote_actor=remote_actor['ident'])
-    return actor_strategies.actors.Actor(strategy=strategy)
+    return actor_strategies.actors.Actor(strategy=strategy, actor_id=remote_actor['ident'])
 
 
 def get_strategy(remote_actor):
